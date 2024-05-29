9c6600940d1b8586a717601c98a8ad134a7226ba
Leigh Engel <leigh.engel@kellogg.northwestern.edu>
2024-05-29 17:13:00 +0000

    Worked on sessions_controller (added from authentication class resource page)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if @user["password"] == params["password"]
+        # login the user
+        flash["notice"] = "You've logged in."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
