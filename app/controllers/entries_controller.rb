e05b21d3ffaa83645513cd239346b89af5b8ee0f
Leigh Engel <leigh.engel@kellogg.northwestern.edu>
2024-06-04 23:24:43 +0000

    uploaded images

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,19 @@
 class EntriesController < ApplicationController
 
-  def new
-  end
 
-  def create
+def create
+      @user = User.find_by({ "id" => session["user_id"] })
+      if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
+    @post.uploaded_image.attach(params["uploaded_image"])
+    @post["user_id"] = @user["id"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+      end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,21 @@ Rails.application.routes.draw do
   get("/login", { :controller => "sessions", :action => "new" })
   get("/logout", { :controller => "sessions", :action => "destroy" })
 
+#   Rails.application.routes.draw do
+#     root "places#index"
+    
+#     resources :entries
+#     resources :places
+#     resources :users
+#     resources :sessions, only: [:new, :create, :destroy]
+  
+#     # Adding the posts resource
+#     resources :posts
+  
+#     get '/login', to: 'sessions#new', as: 'login'
+#     get '/logout', to: 'sessions#destroy', as: 'logout'
+#   end
+  
 
 end
 
